ad616d8c72a38dccfeceaa1c20e3e421eaaf4915
ezchamp <emile269@gmail.com>
2020-09-09 22:10:48 +0000

    Added constants and print statements

--- main.swift
+++ main.swift
@@ -42,9 +42,15 @@ let circumference = 2 * pi * radius
 print(diameter)
 print(circumference)
 
-
 // Your fitness tracker keeps track of users' heart rate, but you might also want to display their average heart rate over the last hour. Create three constants, heartRate1, heartRate2, and heartRate3. Give each constant a different value between 60 and 100. Create a constant addedHR equal to the sum of all three heart rates. Now create a constant called averageHR that equals addedHR divided by 3 to get the average. Print the result.
 
+let heartRate1 = 65
+let heartRate2 = 75
+let heartRate3 = 85
+let addedHR = (heartRate1 + heartRate2 + heartRate3)
+let averageHR = addedHR / 3
+print(addedHR)
+print(averageHR)
 
 // Now create three more constants, heartRate1D, heartRate2D, and heartRate3D, equal to the same values as heartRate1, heartRate2, and heartRate3. These new constants should be of type Double. Create a constant addedHRD equal to the sum of all three heart rates. Create a constant called averageHRD that equals the addedHRD divided by 3 to get the average of your new heart rate constants. Print the result. Does this differ from your previous average? Why or why not?
 
