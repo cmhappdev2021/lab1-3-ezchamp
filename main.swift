30e23d8b946af8d3091b2a6a80cf7f115b1c4c46
ezchamp <emile269@gmail.com>
2020-09-09 22:25:53 +0000

    Added variable and compound assignment operators

--- main.swift
+++ main.swift
@@ -72,6 +72,10 @@ print("\(percentOfGoal)%")
 
 // Declare a variable whose value begins at 10. Using addition, update the value to 15 using the compound assignment operator. Using multiplication, update the value to 30 using compound assignment. Print out the variable's value after each assignment.
 
+var jimmyNeutron = 10
+jimmyNeutron += 5
+jimmyNeutron *= 2
+print(jimmyNeutron)
  
 
 /* Create a variable called piggyBank that begins at 0. You will use this to keep track of money you earn and spend. For each point below, use the right compound assignment operator to update the balance in your piggy bank.
