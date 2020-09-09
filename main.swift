ed4ece5454db5352934933dd6d328942e6a9029d
ezchamp <emile269@gmail.com>
2020-09-09 18:28:34 +0000

    Created a variable and a print statement

--- main.swift
+++ main.swift
@@ -7,6 +7,8 @@ print(area)
 
 // You decide that you'll divide your shed into two rooms. You want to know if dividing it equally will leave enough room for some of your larger storage items. Create a roomArea constant that is the result of dividing area in half. Print out the result.
 
+let roomArea = area / 2
+print(roomArea)
 
 // Create a perimeter constant whose value equals width plus width plus height plus height, then print out the result.
 
