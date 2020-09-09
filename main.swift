0911779276b084e990a401deaf03f0e6a8ae1523
ezchamp <emile269@gmail.com>
2020-09-09 22:15:34 +0000

    Added constants and print statements

--- main.swift
+++ main.swift
@@ -54,6 +54,14 @@ print(averageHR)
 
 // Now create three more constants, heartRate1D, heartRate2D, and heartRate3D, equal to the same values as heartRate1, heartRate2, and heartRate3. These new constants should be of type Double. Create a constant addedHRD equal to the sum of all three heart rates. Create a constant called averageHRD that equals the addedHRD divided by 3 to get the average of your new heart rate constants. Print the result. Does this differ from your previous average? Why or why not?
 
+let heartRate1D:Double = 65
+let heartRate2D:Double = 75
+let heartRate3D:Double = 85
+let addedHRD =  (heartRate1D + heartRate2D + heartRate3D)
+let averageHRD = addedHRD / 3
+print(addedHRD)
+print(averageHRD)
+print("The average answer technically does differ from the previous average because it doesn't truncate the decimal; however, my chosen values resulted in the same average mathematically.")
 
 // Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. Create constants steps and goal. Both will need to be of type Double so that you can perform accurate calculations. steps should be assigned the value 3,467, and goal should be assigned 10,000. Create a constant percentOfGoal that equals an expression that evaluates to the percent of the goal that has been achieved so far.
 
