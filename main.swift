a4393a4c566bf8c8b8e92d1cf9961f74b1319278
ezchamp <emile269@gmail.com>
2020-09-09 22:23:29 +0000

    Added print statement

--- main.swift
+++ main.swift
@@ -68,10 +68,11 @@ print("The average answer technically does differ from the previous average beca
 let steps:Double = 3467
 let goal:Double = 10000
 let percentOfGoal = (steps/goal) * 100
-
+print("\(percentOfGoal)%")
 
 // Declare a variable whose value begins at 10. Using addition, update the value to 15 using the compound assignment operator. Using multiplication, update the value to 30 using compound assignment. Print out the variable's value after each assignment.
 
+ 
 
 /* Create a variable called piggyBank that begins at 0. You will use this to keep track of money you earn and spend. For each point below, use the right compound assignment operator to update the balance in your piggy bank.
 
