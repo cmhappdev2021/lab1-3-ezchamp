86ee65c51bbca949489ce09edb96a0e47374257a
ezchamp <emile269@gmail.com>
2020-09-10 01:05:25 +0000

    added print statements

--- main.swift
+++ main.swift
@@ -120,8 +120,12 @@ print(10+2*5)
 
 // In a separate print statement, add in the necessary parentheses so that addition takes place before multiplication.
 
+print((10+2)*5)
+
 // Print out what you think 4 * 9 - 6 / 2 evaluates to. Then print out the actual expression.
 
+print("33")
+print(4*9-6/2)
 
 // In a separate print statement, add in the necessary parentheses so that the subtraction is prioritized over the multiplication and division.
 
