9313345fb9ed01797693b05a7289fb04796d5c19
ezchamp <emile269@gmail.com>
2020-09-10 01:21:52 +0000

    Added constants and print statements

--- main.swift
+++ main.swift
@@ -150,6 +150,10 @@ print(tempInCelsius)
 
 // Create an integer constant x with a value of 10, and a double constant y with a value of 3.2. Create a constant multipliedAsIntegers equal to x times y. Does this compile? If not, fix it by converting your Double to an Int in the mathematical expression. Print the result.
 
+let x:Int = 10
+let y:Double = 3.2
+let multipliedAsIntegers = x * Int(y)
+print(multipliedAsIntegers)
 
 // Create a constant multipliedAsDoubles equal to x times y, but this time convert the Int to a Double in the expression. Print the result.
 
