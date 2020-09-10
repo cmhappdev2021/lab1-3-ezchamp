c3ab6c45e73fecbd39b95d679a39a47c2fe252a1
ezchamp <emile269@gmail.com>
2020-09-10 01:10:36 +0000

    Added constants and print statements

--- main.swift
+++ main.swift
@@ -129,9 +129,15 @@ print(4*9-6/2)
 
 // In a separate print statement, add in the necessary parentheses so that the subtraction is prioritized over the multiplication and division.
 
+print(4*(9-6)/2)
 
 // If you completed the Fitness Calculations exercise, you calculated an average heart rate to display to the user. However, using proper order of operations you can do this in fewer steps. Create three separate heart rate constants, all of type Double, with values between 60 and 100. Then create a constant equal to the average heart rate. If you use correct order of operations you can do the heart calculation in one line.
 
+let heartbeatRate:Double = 82
+let heartbeatRate2:Double = 68
+let heartbeatRate3:Double = 74
+let averageHeartBeatRate = (heartbeatRate + heartbeatRate2 + heartbeatRate3) / 3
+print(averageHeartBeatRate)
 
 // One feature you might want to give users is to display their current body temperature. Create a constant tempInFahrenheit equal to 98.6.
 
