187c94e748c454cd0766e36d7e05ff747766fd87
ezchamp <emile269@gmail.com>
2020-09-10 01:11:38 +0000

    Added constant

--- main.swift
+++ main.swift
@@ -141,6 +141,7 @@ print(averageHeartBeatRate)
 
 // One feature you might want to give users is to display their current body temperature. Create a constant tempInFahrenheit equal to 98.6.
 
+let tempInFahrenheit = 98.6
 
 // You may want to also show the temperature in celsius. You can convert fahrenheit to celsius by taking tempInFahrenheit and subtracting 32, then multiplying the result by (5.0/9.0). Create a constant tempInCelsius that calculates in one line the temperature in celsius.
 
