d3be682247121a6ae13eba5cb8271a2031957d41
ezchamp <emile269@gmail.com>
2020-09-10 01:22:43 +0000

    added constant and print statement

--- main.swift
+++ main.swift
@@ -157,6 +157,8 @@ print(multipliedAsIntegers)
 
 // Create a constant multipliedAsDoubles equal to x times y, but this time convert the Int to a Double in the expression. Print the result.
 
+let multipliedAsDoubles = Double(x) * y
+print(multipliedAsDoubles)
 
 // Are the values of multipliedAsIntegers and multipliedAsDoubles different? Print a statement to the console explaining why.
 
